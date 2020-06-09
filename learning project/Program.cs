6bfb3ce16f859dd19a2498317d68ed04d21e4a49
SURYAJITH NAIR <suryajith082@gmail.com>
2020-06-09 20:56:38 +0530

    Add project files.

---
+++ learning project/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+namespace learning_project
+{
+    class Program
+    {
+        [DllImport("C:\\Users\\Acer PC\\source\\repos\\templateMatching\\x64\\Debug\\templateMatching.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int res([MarshalAs(UnmanagedType.LPStr)] String a, [MarshalAs(UnmanagedType.LPStr)]String b);
+        public static void Main()
+        {
+            int ret = res("C:\\Users\\Acer PC\\Desktop\\New folder\\search.jpg", "C:\\Users\\Acer PC\\Desktop\\New folder\\google.jpg");
+            Console.WriteLine("Return Value: " + ret);
+            Console.ReadLine();
+        }
+    }
+}
