1f12397cc2d1f2c4d9129a9a6f98ef15c6e659ea
SURYAJITH NAIR <suryajith082@gmail.com>
2020-06-13 20:56:10 +0530

    added setcursorpos function and mouse event function from user32.dll dynamic library.

--- learning project/Program.cs
+++ learning project/Program.cs
@@ -4,17 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
 namespace learning_project
 {
     class Program
     {
+        [DllImport("C:\\WINDOWS\\System32\\user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, uint dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const int MOUSEEVENTF_LEFTUP = 0x0004;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        private const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        private const int MOUSEEVENTF_MOVE= 0x0001;
+        private const int MOUSEEVENTF_ABSOLUTE= 0x8000;
+
+        [DllImport("C:\\WINDOWS\\System32\\user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool SetCursorPos(int X,  int Y);
+
+
         [DllImport("C:\\Users\\Acer PC\\source\\repos\\templateMatching\\x64\\Debug\\templateMatching.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern int res([MarshalAs(UnmanagedType.LPStr)] String a, [MarshalAs(UnmanagedType.LPStr)]String b);
         public static void Main()
         {
-            int ret = res("C:\\Users\\Acer PC\\Desktop\\New folder\\search.jpg", "C:\\Users\\Acer PC\\Desktop\\New folder\\google.jpg");
+            Console.WriteLine("Starting the process...");
+            Thread.Sleep(5000);
+
+            //Creating a Rectangle object which will  
+            //capture our Current Screen
+            Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
+            //Creating a new Bitmap object
+            Bitmap captureBitmap = new Bitmap(captureRectangle.Right- captureRectangle.Left, captureRectangle.Bottom- captureRectangle.Top, PixelFormat.Format32bppArgb);
+            //Bitmap captureBitmap = new Bitmap(int width, int height, PixelFormat);
+            //Creating a New Graphics Object
+            Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+            //Copying Image from The Screen
+            captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+            //Saving the Image File
+
+            captureBitmap.Save(@"C:\Users\Acer PC\Desktop\New folder\Capture.jpg", ImageFormat.Jpeg);
+
+            int ret = res("C:\\Users\\Acer PC\\Desktop\\New folder\\Capture.jpg", "C:\\Users\\Acer PC\\Desktop\\New folder\\newgoogle.jpg");
             Console.WriteLine("Return Value: " + ret);
             Console.ReadLine();
+            SetCursorPos(500, 500);
+            mouse_event( MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+            Console.ReadLine();
+            Console.ReadLine();
+            File.Delete("C:\\Users\\Acer PC\\Desktop\\New folder\\Capture.jpg");
+            
         }
     }
 }
